458af57b29b900dc0aefeba8f26070a570089ad4
max <max@vater.com>
2021-12-30 00:34:06 +0100

    E2E Test für AuthController

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, ValidationPipe } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
@@ -6,6 +6,10 @@ import { ReportsModule } from './reports/reports.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './users/user.entity';
 import { Report } from './reports/report.entity';
+import { APP_PIPE } from '@nestjs/core';
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const cookieSession = require('cookie-session');
 
 @Module({
   imports: [
@@ -19,6 +23,27 @@ import { Report } from './reports/report.entity';
     }),
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      //Global Pipe
+      provide: APP_PIPE, //alle requests werden durch APP_PIPE an ValidationPipe weitergegeben
+      useValue: new ValidationPipe({
+        whitelist: true, //erlaubt nur was in den DTO's steht
+      }),
+    },
+  ],
 })
-export class AppModule {}
+export class AppModule {
+  configure(consumer: MiddlewareConsumer) {
+    console.log('APP_MODULE: ');
+    //Global Middleware
+    consumer
+      .apply(
+        cookieSession({
+          keys: ['asdadawdawdwf'],
+        }),
+      )
+      .forRoutes('*');
+  }
+}
